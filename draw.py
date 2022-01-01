1ab3a39b96a67c626408c977a8c398ec893efd79
許宛筑 <enfyshsu@xuwanzhus-MacBook-Pro.local>
2022-01-01 12:20:57 +0800

    update font size

--- draw.py
+++ draw.py
@@ -8,7 +8,7 @@ def cellular_usage_1(title, x_name, y_name, data):
     proposed = [29, 29, 30]
     MDP = [45, 70, 110]
     plot(title, x_name, y_name, data, min_cost, min_delay, proposed, MDP)
-    plt.savefig("result/1_cellular_usage.png", dpi=600)
+    plt.savefig("result/1_cellular_usage.png", bbox_inches='tight', dpi=600, pad_inches=0.1)
     
 def finished_time_2(title, x_name, y_name, data):
     min_cost = [139, 343, 643]
@@ -16,7 +16,7 @@ def finished_time_2(title, x_name, y_name, data):
     proposed = [103, 173, 402]
     MDP = [125, 185, 460]
     plot(title, x_name, y_name, data, min_cost, min_delay, proposed, MDP)
-    plt.savefig("result/2_finished_time.png", dpi=600)
+    plt.savefig("result/2_finished_time.png", bbox_inches='tight', dpi=600, pad_inches=0.1)
 
 
 # Cellular Usage Change Data Rate
@@ -26,7 +26,7 @@ def cellular_usage_3(title, x_name, y_name, data):
     proposed = [29, 50, 51, 52, 50]
     MDP = [45, 45, 60, 85, 80]
     plot(title, x_name, y_name, data, min_cost, min_delay, proposed, MDP)
-    plt.savefig("result/3_cellular_usage.png", dpi=600)
+    plt.savefig("result/3_cellular_usage.png", bbox_inches='tight', dpi=600, pad_inches=0.1)
 
 # Finished Time
 def finished_time_4(title, x_name, y_name, data):
@@ -35,18 +35,18 @@ def finished_time_4(title, x_name, y_name, data):
     proposed = [103, 46, 46, 46, 46]
     MDP = [125, 115, 110, 70, 85]
     plot(title, x_name, y_name, data, min_cost, min_delay, proposed, MDP)
-    plt.savefig("result/4_finished_time.png", dpi=600)
+    plt.savefig("result/4_finished_time.png", bbox_inches='tight', dpi=600, pad_inches=0.1)
 
 def plot(title, x_name, y_name, data, min_cost, min_delay, proposed, MDP):
-    plt.title(title, fontweight = "bold")
-    plt.xlabel(x_name)
-    plt.ylabel(y_name)
-    plt.xticks(data)
+    plt.title(title, fontweight = "bold", fontsize=16)
+    plt.xlabel(x_name, fontsize=16)
+    plt.ylabel(y_name, fontsize=16)
+    plt.xticks(data, fontsize=16)
     plt.plot(data, min_delay, marker='.', label = "min_delay")
     plt.plot(data, min_cost, marker='.', label = "min_cost")
     plt.plot(data, proposed, marker='.', label = "proposed")
     plt.plot(data, MDP, marker='.', label = "MDP")
-    plt.legend(labels=["min_delay", "min_cost", "without V2V", "MDP"], loc = 'best')
+    plt.legend(labels=["Minimal delay", "Minimal cost", "Without V2V", "MDP"], loc = 'best')
 
 if __name__ == '__main__':
     data_size = [100, 150, 200]
